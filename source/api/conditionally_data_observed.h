ef0fb198e2778b360bafbda89185b5f6c9f4d42c
Gustavo D'Angelo <gustavo.vonn@gmail.com>
2025-08-22 17:59:11 -0300

    refact: observer

---
+++ source/api/conditionally_data_observed.h
@@ -0,0 +1,10 @@
+#ifndef CONDITIONALLY_DATA_OBSERVED_H
+#define CONDITIONALLY_DATA_OBSERVED_H
+
+template <typename T, typename Condition = void>
+class Conditionally_Data_Observed
+{
+    // To define...
+};
+
+#endif // CONDITIONALLY_DATA_OBSERVED_H
