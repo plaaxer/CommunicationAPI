6114aca41a5e473957bdc0ed5e6da830a793de17
FelipeGassen <felipethaddeugassen@gmail.com>
2025-08-22 17:12:22 -0300

    added vehicle and sensor

--- source/vehicle.cpp
+++ source/vehicle.cpp
@@ -1,6 +1,11 @@
 #include<cstdlib>
 #include<ctime>
+#include "sensor.cpp"
+#include "communicator.h"
 
 class Vehicle {
-
+    private:
+        Sensor velocimetro;
+    public:
+        Vehicle(Sensor v);
 };
