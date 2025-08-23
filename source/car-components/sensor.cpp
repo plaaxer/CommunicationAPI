4b52d3ccb42f53032709cb87f3ba8ba8f8cc1f6d
FelipeGassen <felipethaddeugassen@gmail.com>
2025-08-23 15:19:46 -0300

    vehicle and sensor

--- source/car-components/sensor.cpp
+++ source/car-components/sensor.cpp
@@ -1,12 +1,27 @@
 #include<cstdlib>
 #include<ctime>
+#include <unistd.h>
 
 class Sensor {
     private: 
+        int type = 0;
         float reading;
     public:
         float get_reading();
         void update();
+        void start();
+        pid_t pid;
+};
+
+void Sensor::start(){
+    pid = fork();
+    if (pid < 0) {
+        return;
+    } else if (pid == 0){
+        while(true) {
+            update();
+        };
+    };
 };
 
 float Sensor::get_reading() {
--- source/vehicle.cpp
+++ source/vehicle.cpp
@@ -1,10 +1,43 @@
-#include<cstdlib>
-#include<ctime>
-#include "sensor.cpp"
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "car-components/sensor.cpp"
+#include "car-components/message-controller.cpp"
+
+
+//TODO: 
+// - Get MAC Address from machine (Done)
+// - Include controller for communicating with message API 
 
 class Vehicle {
     private:
         Sensor velocimetro;
+        float speed;
+        std::string address;
     public:
-        Vehicle(Sensor v);
+        Vehicle();
+        void update_sensor();
+        void get_address();
+        void send_message();
 };
+
+Vehicle::Vehicle(){
+    get_address();
+    Sensor *velocimetro = new Sensor();
+};
+
+void Vehicle::get_address(){
+    std::ifstream file("/sys/class/net/eth0/address");
+    if (!file.is_open()) {
+        return;
+    }
+
+    std::string mac;
+    std::getline(file, mac); // read first line
+    file.close();
+    address = mac;
+};
+
