06ea33dd53b13cb399d2069edf39fe425d8e1841
FelipeGassen <felipethaddeugassen@gmail.com>
2025-08-23 23:26:05 -0300

    observers first draft

--- source/api/observer/conditional_data_observer.h
+++ source/api/observer/conditional_data_observer.h
@@ -1,10 +1,18 @@
 #ifndef CONDITIONAL_DATA_OBSERVER_H
 #define CONDITIONAL_DATA_OBSERVER_H
 
+template <typename T, typename Condition>
+class Conditionally_Data_Observed;
+
 template <typename T, typename Condition = void>
-class Conditional_Data_Observer
-{
-    // To define...
+class Conditional_Data_Observer {
+    friend class Conditionally_Data_Observed<T, Condition>;
+
+    public:
+        typedef T ObsData;
+        typedef Condition ObsCondition;
+
+        virtual void update(Conditionally_Data_Observed<T, Condition> *obs, Condition c, T *d)
 };
 
 #endif // CONDITIONAL_DATA_OBSERVER_H
--- source/api/observer/conditionally_data_observed.h
+++ source/api/observer/conditionally_data_observed.h
@@ -1,10 +1,47 @@
 #ifndef CONDITIONALLY_DATA_OBSERVED_H
 #define CONDITIONALLY_DATA_OBSERVED_H
+#include<list>
+
+template <typename T, typename Condition>
+class Conditional_Data_Observer;
 
 template <typename T, typename Condition = void>
 class Conditionally_Data_Observed
 {
-    // To define...
+    friend class Conditional_Data_Observer<T, Condition>;
+
+    public:
+        typedef T obs_data;
+        typedef Conditional_Data_Observer<T, Condition> Observer;
+        typedef std::list<Observer *> ObsList;
+        typedef std::list<Condition> ConList;
+
+        virtual void attach(Observer * o, Condition c) {
+            _observers.push_front(o);
+            _conditions.push_front(c);
+        }
+
+        virtual void dettach(Observer * o, Condition c) {
+            _observers.remove(o);
+            _conditions.remove(c);
+        }
+
+        virtual bool notify(Condition c, ObsData *d) {
+            bool n = false;
+            std::size_t size = _observers.size();
+            int range = static_cast<int>(size);
+            for(i = 0; i < range; i++) {
+                if (_conditions[i] == c) {
+                    _observers[i]->update(this, c, d);
+                    n = true;
+                }
+            }
+        }
+
+    private:
+        ObsList _observers;
+        ConList _conditions;
+
 };
 
 #endif // CONDITIONALLY_DATA_OBSERVED_H
