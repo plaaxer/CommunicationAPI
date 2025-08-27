bb4cbf135d4747a864d853c389376c36a87d0650
Gustavo D'Angelo <gustavo.vonn@gmail.com>
2025-08-27 05:21:38 -0300

    cleaning unused

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# =========================================================
-# WE'LL USE THIS SCRIPT TO RUN THE VM'S. THERE ARE OPTIONS
-# THAT HELPS US TO SEE THE SYNCHRONOUS EXECUTION IN A VIRTUAL
-# MANAGER.
-# =========================================================
